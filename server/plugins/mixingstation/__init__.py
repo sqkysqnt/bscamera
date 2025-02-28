958e19672acb995d9749d6916b3e28a4d25b39dc
sqkysqnt <39272083+sqkysqnt@users.noreply.github.com>
2025-02-27 20:16:21 -0600

    Create __init__.py

---
+++ server/plugins/mixingstation/__init__.py
@@ -0,0 +1,26 @@
+# plugins/x32_app/__init__.py
+
+import threading
+from plugins.plugin_interface import PluginInterface
+from .plugin import ms_bp, periodic_check
+import os
+import logging
+
+class Plugin(PluginInterface):
+    def register(self, app, socketio, dispatcher):
+        # Register the blueprint
+        app.register_blueprint(ms_bp, url_prefix='/mixingstation')
+
+        # Only start the thread in the true main process
+        if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+            thread = threading.Thread(target=periodic_check, daemon=True)
+            thread.start()
+            logging.info("periodic_check thread started in main process.")
+
+    def get_plugin_info(self):
+        return {
+            "name": "Mixing Station Channel Monitor",
+            "url": "/mixingstation",
+            "description": "Configure Mixing Station channels and send OSC messages.",
+            "ignore": False
+        }
